beb2012ba9ce8a7559141d4c7381fb2320f1ffae
theprimone <theprimone@users.noreply.github.com>
2021-06-12 15:46:21 +0000

    deploy: 44ecfdedbf2c494b067cdac730a74bf397671258

---
+++ assets/BlankLayout.e6cc5ba2.js
@@ -0,0 +1,1 @@
+import{R as e}from"./vendor.bcba5a07.js";export default({children:r})=>e.createElement("div",null,r);
---
+++ assets/index.c5ebd1d8.js
@@ -0,0 +1,1 @@
+import"./index.87115ea7.js";import{C as e}from"./index.fd4e4f47.js";import{R as r}from"./vendor.bcba5a07.js";function n(){const n=r.useRef(0);return n.current+=1,r.createElement(e,null,r.createElement("h2",null,"Applications Search Page")," ",r.createElement("br",null),r.createElement("div",null,"renderCount: ",n.current))}export default n;
