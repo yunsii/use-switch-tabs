6bdcdb0dd261316641f5dd392c774f82bf5d1ef5
theprimone <yuns.xie@qq.com>
2021-06-19 20:59:02 +0800

    feat: export mode

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as H from 'history';
 
 import { default as useSwitchTabs } from './useSwitchTabs';
 import { withSwitchTab } from './utils';
+import { Mode } from './config'
 import type { UseSwitchTabsOptions, ActionType } from './useSwitchTabs';
 
 interface SwitchTab {
@@ -16,6 +17,6 @@ type RoughLocation = Omit<H.Location, 'key'>;
 
 export type { UseSwitchTabsOptions, SwitchTab, RoughLocation, ActionType };
 
-export { useSwitchTabs, withSwitchTab };
+export { useSwitchTabs, withSwitchTab, Mode };
 
 export default useSwitchTabs;
